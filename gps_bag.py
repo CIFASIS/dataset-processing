d12307136b0bc31960fd75ef7c5353ba36a78801
Taihú Pire <pire@cifasis-conicet.gov.ar>
2018-01-22 16:27:27 -0300

    gps rosbag parser created

--- gps_bag.py
+++ gps_bag.py
@@ -1,38 +1,57 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import argparse
+import rospy
 import rosbag
-from std_msgs.msg import Int32, String #not needed, only for testing with basic example
-from nmea_msgs.msg import Sentence
-
-data = Sentence()
-bag = rosbag.Bag('test.bag', 'w')
-F = open("/home/martin/datasets/2017-12-26_11:03:06/gps/2017-12-26_11:03:06.log",'r')
+#from std_msgs.msg import Int32, String #not needed, only for testing with basic example
+from nmea_msgs.msg import Sentence #  sudo apt-get install ros-kinetic-nmea-msgs
 
 
-for line in F:  #for each line in raw file, take each part: time,id,msg
-	data.header.stamp.secs = line[0:10] #spliting seconds
-	data.header.stamp.nsecs = line [11:17] #spliting nanoseconds, is this nanoseconds? or it needs a convertion?
-	data.header.frame_id = line [18:25] #frame_id = GPS-RTK
-	data.sentence = line [28:] # message from the tipe of NMEA sentence
-	print data.header.stamp.secs
-	print data.header.stamp.nsecs
-	print data.header.frame_id
-	print data.sentence
-	bag.write(data.header.frame_id,data.sentence, data.header.stamp)  # Here is the problem, data.sentence doesnt have type
-	
-bag.close()
-F.close()
-quit()
+if __name__ == "__main__":
 
-'''
-a=String()
-a.data = "erfdf"
-print "d3"
-bag.write(line.header.frame_id,a, line.header.stamp)
-print "d4"
-#bag.write('numbers', i)
-bag.close()
-'''
+  parser = argparse.ArgumentParser(description='Script that create a rosbag containing NMEA ROS sentences')
+  parser.add_argument('gps_nmea', help='GPS log file')
+
+  args = parser.parse_args()
+
+  file = open(args.gps_nmea, 'r')
+
+  bag = rosbag.Bag('test.bag', 'w')
+
+  gps_topic = "/gps_rtk"
 
 
+  for line in file:  #for each line in raw file, take each part: time,id,msg
+
+    # get timestamp
+    timestamp = line.split(' ')[0]
+
+    # get seconds
+    seconds = int(timestamp.split('.')[0])
+
+    # get milliseconds and convert them to nanoseconds
+    nanoseconds = int(timestamp.split('.')[1]) * 1000
+
+    # create ros time for the message
+    time = rospy.Time(seconds, nanoseconds)
+
+    # create message
+    data = Sentence()
+
+    # message header
+    data.header.stamp.secs = seconds
+    data.header.stamp.nsecs = nanoseconds
+    data.header.frame_id = line.split(' ')[1][:-1]     #set frame_id = GPS-RTK
+
+    # message data
+    data.sentence = line.split(' ')[2] # message from the type of NMEA sentence
+#    data.sentence = line [28:] # message from the type of NMEA sentence
+#    print data.header.stamp.secs
+#    print data.header.stamp.nsecs
+#    print data.header.frame_id
+#    print data.sentence
+    bag.write(gps_topic, data, time)
+
+  bag.close()
+  file.close()
+  quit()
 
