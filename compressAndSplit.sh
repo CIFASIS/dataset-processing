a629d2d93e9c5227d494d64eb256226fa46b3d0a
taihu <taihu@cifasis-conicet-gov.ar>
2018-08-24 16:24:04 -0300

    Add compressAndSplit.sh script

---
+++ compressAndSplit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for i in `seq 1 1`; do
+    SEQ_NAME=sequence0$i
+    SEQ_DIR=$SEQ_NAME/
+    echo "processing sequence 0"$i
+    # compress the sequence directory 
+    tar cvzf $SEQ_NAME.tar.gz $SEQ_DIR
+
+    # split the compressed sequence dile
+    split --bytes=2GB -d $SEQ_NAME.tar.gz $SEQ_NAME.tar.gz.
+
+    # compress rosbag sequence
+    rosbag compress $SEQ_NAME.bag
+done
