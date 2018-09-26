213a0cdab4e81157046dd1dee3fd613b176eb6d6
Taihú Pire <pire@cifasis-conicet.gov.ar>
2018-09-26 09:56:50 -0300

    Improve compressAndSplit.sh script

--- compressAndSplit.sh
+++ compressAndSplit.sh
@@ -3,30 +3,37 @@
 OUTPUT_DIR=$HOME/datasets/robot_desmalezador
 DATASET_DIR=$HOME/datasets/robot_desmalezador
 
-for i in `seq 4 4`; do
+for i in `seq 1 6`; do
     SEQ_NAME=sequence0$i
     SEQ_DIR=$DATASET_DIR/$SEQ_NAME
-    echo "processing $SEQ_NAME in directory " $DATASET_DIR
+    echo "Processing $SEQ_NAME in directory " $DATASET_DIR/
 
     # generate rosbag
+    echo "Creating $OUTPUT_DIR/$SEQ_NAME.bag"
     python create_bagfile.py --images $SEQ_DIR/zed/ --imu $SEQ_DIR/imu.log --gps $SEQ_DIR/gps.log --calibration $SEQ_DIR/calibration.yaml --odom $SEQ_DIR/odometry_raw.log --out $OUTPUT_DIR/$SEQ_NAME.bag
 
     # compress the sequence directory 
-    tar cvzf $SEQ_NAME.tar.gz $SEQ_DIR
+    echo "Compressing $SEQ_DIR"
+    tar cf - $SEQ_DIR -P | pv -s $(du -sb $SEQ_DIR | awk '{print $1}') | gzip > $OUTPUT_DIR/$SEQ_NAME.tar.gz
 
     # compress rosbag sequence
-    rosbag compress $SEQ_NAME.bag
+    echo "Compressing $OUTPUT_DIR/$SEQ_NAME.bag"
+    rosbag compress $OUTPUT_DIR/$SEQ_NAME.bag
 
     if [ "$i" = "1" ] || [ "$i" = "2" ] || [ "$i" = "5" ] || [ "$i" = "6" ]; then
 
       # split the compressed sequence file
-      split --bytes=2GB -d $SEQ_NAME.tar.gz $SEQ_NAME.tar.gz.
+      echo "Splitting $OUTPUT_DIR/$SEQ_NAME.tar.gz"
+      split --bytes=2GB -d $OUTPUT_DIR/$SEQ_NAME.tar.gz $OUTPUT_DIR/$SEQ_NAME.tar.gz.
 
       # split the compressed rosbag
-      split --bytes=2GB -d $SEQ_NAME.bag $SEQ_NAME.bag.
+      echo "Splitting $OUTPUT_DIR/$SEQ_NAME.bag"
+      split --bytes=2GB -d $OUTPUT_DIR/$SEQ_NAME.bag $OUTPUT_DIR/$SEQ_NAME.bag.
     fi
 
     # generate checksum
-    md5sum $SEQ_NAME.tar.gz >> checksum.txt
-    md5sum $SEQ_NAME.bag >> checksum.txt
+    echo "Computing checksums"
+    md5sum $OUTPUT_DIR/$SEQ_NAME.tar.gz >> $OUTPUT_DIR/checksum.txt
+    md5sum $OUTPUT_DIR/$SEQ_NAME.bag >> $OUTPUT_DIR/checksum.txt
+    echo "Everything done"
 done
