94d1b934c210f98d79b21fe312383595c120a287
taihu <taihu@cifasis-conicet-gov.ar>
2018-11-28 13:57:09 -0300

    Add converter gt2kitti.py

---
+++ data_converters/gt2kitti.py
@@ -0,0 +1,50 @@
+import argparse
+import logging
+import csv
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+def main():
+  """Main."""
+
+  parser = argparse.ArgumentParser()
+  parser.description = "Convert dataset ground-truth positions to KITTI format"
+  parser.add_argument(
+    '-g',
+    '--dataset-gt',
+    required=True,
+    help=("Ground-Truth positions (format: timestamp x y z)"))
+
+  # parse cmdline args
+  parser_args = vars(parser.parse_args())
+  in_dataset_gt_file = parser_args["dataset_gt"]
+
+  # Ground-Truth file
+  # Convert it to a
+  if in_dataset_gt_file is None:
+      logger.warn("Ground-Truth file not provided")
+      return
+  else:
+      logger.info("moving the Ground-Truth information...")
+
+      with open(in_dataset_gt_file) as inputFile:
+        with open('output.txt', 'w') as outputFile:
+          writer = csv.writer(outputFile, delimiter=' ')
+          reader = csv.reader(inputFile, delimiter=' ')
+          for line in reader:
+            timestamp = line[0]
+            x_pos = line[1]
+            y_pos = line[2]
+            z_pos = line[3]
+
+            # create kitti pose with identity rotation matrix
+            newRow = [1.0, 0.0 , 0.0, x_pos, 0.0, 1.0, 0.0, y_pos, 0.0, 0.0, 1.0, z_pos]
+            writer.writerow(newRow)
+
+      # close files
+      inputFile.close()
+      outputFile.close()
+
+if __name__=="__main__":
+  main()
