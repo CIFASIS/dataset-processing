5aa9b2559b12ac7cb33a848ae1ac52d36edb3599
taihu <taihu@cifasis-conicet-gov.ar>
2018-12-06 16:09:06 -0300

    Improve nmea2gps script

--- nmea2gps.py
+++ nmea2gps.py
@@ -6,76 +6,75 @@ from sensor_msgs.msg import NavSatFix, NavSatStatus
 
 
 def get_gps_data_fromGGA(line):
-  timestamp = line.split(' ')[0]
-  seconds = timestamp.split('.')[0]
-  nanoseconds = timestamp.split('.')[1] + "000" 
-  sentencesData = line.split(',')
+    timestamp = line.split(' ')[0]
+    seconds = timestamp.split('.')[0]
+    nanoseconds = timestamp.split('.')[1] + "000"
+    sentencesData = line.split(',')
 
-  #get status and service
-  gps_qual = int(sentencesData[6])
-  if gps_qual == 0:
-    status = NavSatStatus.STATUS_NO_FIX
-  elif gps_qual == 1:
-    status = NavSatStatus.STATUS_FIX
-  elif gps_qual == 2:
-    status = NavSatStatus.STATUS_SBAS_FIX
-  elif gps_qual in (4, 5):
-    status = NavSatStatus.STATUS_GBAS_FIX
-  elif gps_qual == 9:
-    # Support specifically for NOVATEL OEM4 recievers which report WAAS fix as 9
-    # http://www.novatel.com/support/known-solutions/which-novatel-position-types-correspond-to-the-gga-quality-indicator/
-    status = NavSatStatus.STATUS_SBAS_FIX
-  else:
-    status = NavSatStatus.STATUS_NO_FIX
-  service = NavSatStatus.SERVICE_GPS	
+    #get status and service
+    gps_qual = int(sentencesData[6])
+    if gps_qual == 0:
+        status = NavSatStatus.STATUS_NO_FIX
+    elif gps_qual == 1:
+        status = NavSatStatus.STATUS_FIX
+    elif gps_qual == 2:
+        status = NavSatStatus.STATUS_SBAS_FIX
+    elif gps_qual in (4, 5):
+        status = NavSatStatus.STATUS_GBAS_FIX
+    elif gps_qual == 9:
+        # Support specifically for NOVATEL OEM4 recievers which report WAAS fix as 9
+        # http://www.novatel.com/support/known-solutions/which-novatel-position-types-correspond-to-the-gga-quality-indicator/
+        status = NavSatStatus.STATUS_SBAS_FIX
+    else:
+        status = NavSatStatus.STATUS_NO_FIX
+    service = NavSatStatus.SERVICE_GPS
 
+    # get latitude in degrees
+    latitudeRaw = float(sentencesData[2])
+    latitudeRawDegrees = latitudeRaw // 100 # int type division
+    latitudeRawMinutes = latitudeRaw % 100
+    # Get the sign of the latitude. It depends if latitude is North or South
+    latitudeSign = 1
+    latitudeCartidnalDirection = sentencesData[3]
+    if latitudeCartidnalDirection == 'S':
+        latitudeSign = -1
 
-  # get latitude in degrees
-  latitudeRaw = float(sentencesData[2])
-  latitudeRawDegrees = latitudeRaw // 100 # int type division
-  latitudeRawMinutes = latitudeRaw % 100
-  # Get the sign of the latitude. It depends if latitude is North or South
-  latitudeSign = 1
-  latitudeCartidnalDirection = sentencesData[3]
-  if latitudeCartidnalDirection == 'S':
-    latitudeSign = -1
+    latitude = latitudeSign * (latitudeRawDegrees + latitudeRawMinutes / 60.0)
 
-  latitude = latitudeSign * (latitudeRawDegrees + latitudeRawMinutes / 60.0)
+    # get longitude in degrees
+    longitudeRaw = float( sentencesData[4] )
+    longitudeRawDegrees = longitudeRaw // 100 # division entera
+    longitudeRawMinutes = longitudeRaw % 100
 
-  # get longitude in degrees
-  longitudeRaw = float( sentencesData[4] )
-  longitudeRawDegrees = longitudeRaw // 100 # division entera
-  longitudeRawMinutes = longitudeRaw % 100
+    # Get the sign of the longitude. It depends if longitude is West or East
+    longitudeSign = 1
+    longitudeCartidnalDirection = sentencesData[5]
 
-  # Get the sign of the longitude. It depends if longitude is West or East
-  longitudeSign = 1
-  longitudeCartidnalDirection = sentencesData[5]
+    if longitudeCartidnalDirection == 'W':
+        longitudeSign = -1
 
-  if longitudeCartidnalDirection == 'W':
-    longitudeSign = -1
+    longitude = longitudeSign * (longitudeRawDegrees + longitudeRawMinutes / 60.0)
 
-  longitude = longitudeSign * (longitudeRawDegrees + longitudeRawMinutes / 60.0)
+    # get altitude in meters (9 is above sea level, 11 is sea level above ellipsoide) with 0 reference at the ellipsoide
+    altitude = float(sentencesData[9]) + float(sentencesData[11])
 
-  # get altitude in meters (9 is above sea level, 11 is sea level above ellipsoide) with 0 reference at the ellipsoide
-  altitude = float(sentencesData[9]) + float (sentencesData[11])
+    # get covariance using Horizontal dilution of position
+    hdop = float(sentencesData[8])
+    position_covariance = [0,0,0,0,0,0,0,0,0]
+    position_covariance[0] = hdop ** 2
+    position_covariance[1] = 0.0
+    position_covariance[2] = 0.0
+    position_covariance[3] = 0.0
+    position_covariance[4] = hdop ** 2
+    position_covariance[5] = 0.0
+    position_covariance[6] = 0.0
+    position_covariance[7] = 0.0
+    position_covariance[8] = (2 * hdop) ** 2
+    position_covariance_type = NavSatFix.COVARIANCE_TYPE_APPROXIMATED
 
-  # get covariance using Horizontal dilution of position
-  hdop = float(sentencesData[8])
-  position_covariance = [0,0,0,0,0,0,0,0,0]
-  position_covariance[0] = hdop ** 2
-  position_covariance[1] = 0.0
-  position_covariance[2] = 0.0
-  position_covariance[3] = 0.0
-  position_covariance[4] = hdop ** 2
-  position_covariance[5] = 0.0
-  position_covariance[6] = 0.0
-  position_covariance[7] = 0.0
-  position_covariance[8] = (2 * hdop) ** 2
-  position_covariance_type = NavSatFix.COVARIANCE_TYPE_APPROXIMATED
+    return int(seconds), int(nanoseconds), status, service, latitude, longitude, altitude, position_covariance, position_covariance_type
 
-  return int(seconds), int(nanoseconds), status, service, latitude, longitude, altitude, position_covariance, position_covariance_type
-
-def get_position(latitude,longitude,gps_secs,gps_nsecs):
+def get_position(latitude, longitude, altitude, gps_secs,gps_nsecs):
 
     global initialx
     global initialy
@@ -96,21 +95,35 @@ def get_position(latitude,longitude,gps_secs,gps_nsecs):
     return timestamp, gps_x ,gps_y, gps_z 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Script that takes the raw gps data and cereates a file with timestamp x y z ')
-    parser.add_argument('--output', help='out file')
-    parser.add_argument('--input', help='raw gps data with nmea sentences')
+
+
+    parser = argparse.ArgumentParser()
+    parser.description = "Script that takes the raw gps data and creates a file with timestamp x y z"
+    parser.add_argument(
+        '-i',
+        '--input',
+        required=True,
+        help="raw gps data with nmea sentences")
+
+    parser.add_argument(
+        '-o',
+        '--output',
+        required=False,
+        help="out file in TUM format")
     args = parser.parse_args()
-    
+
     initialx = 0
     initialy = 0
     initialz = 0
-    
-    f_in = open(args.input,'rw')
-    f_out = open(args.output,'w+')
+
+    f_in = open(args.input, 'rw')
+    f_out = open(args.output, 'w+')
 
     seq_GGA = 0
-    for line in f_in:	
+    for line in f_in:
         if "GGA" in line:
-            seconds, nanoseconds, status, service, latitude, longitude, altitude, position_covariance, position_covariance_type = get_gps_data_fromGGA(line)
-            timestamp, x, y ,z = get_position(longitude,latitude,seconds,nanoseconds)
-            f_out.write(str(timestamp) + ' ' + str(x)+ ' ' + str(y) + ' ' + str(z) + ' ' + "0" + ' ' + "0" + ' ' + "0" + ' ' + "1"+ "\r\n") 
+            seconds, nanoseconds, status, service, latitude, longitude, altitude, position_covariance, position_covariance_type = get_gps_data_fromGGA(
+                line)
+            timestamp, x, y, z = get_position(longitude, latitude, altitude, seconds, nanoseconds)
+            f_out.write(str(timestamp) + ' ' + str(x) + ' ' + str(y) + ' ' + str(
+                z) + ' ' + "0" + ' ' + "0" + ' ' + "0" + ' ' + "1" + "\r\n")
