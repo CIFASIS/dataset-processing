1b654dd5442633be794e93f9efefa7fce8add5d8
taihu <taihu@cifasis-conicet-gov.ar>
2018-12-14 08:57:38 -0300

    Improve kitti_gt2tum.py script

--- data_converters/kitti_gt2tum.py
+++ data_converters/kitti_gt2tum.py
@@ -9,6 +9,7 @@ import sys
 sys.path.insert(0, '../')
 from utils import transformations as tf
 from utils.Time import Time
+import rosbag
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -24,10 +25,10 @@ def main():
     required=True,
     help=("KITTI ground-truth file (format: r00 r01 r02 tx r10 r11 r12 ty r20 r21 r22 tz)"))
   parser.add_argument(
-    '-t',
-    '--times-file',
+    '-r',
+    '--rosbag-file',
     required=True,
-    help=("KITTI times.txt file (format: seconds)"))
+    help=("Rosbag file to extract the timestamp of frames."))
   parser.add_argument(
       '-o',
       '--output',
@@ -38,7 +39,7 @@ def main():
   # parse cmdline args
   parser_args = vars(parser.parse_args())
   in_kitti_gt_file = parser_args["kitti_gt"]
-  in_times_file = parser_args["times_file"]
+  in_rosbag_file = parser_args["rosbag_file"]
   out_new_format = parser_args["output"]
 
   # Convert KITTI ground-truth with times file to TUM format
@@ -48,21 +49,22 @@ def main():
   else:
       logger.info("Converting KITTI ground-truth file to TUM format...")
 
+      # get timestamps from rosbag
+      timestamps = []
+      for topic, msg, t in rosbag.Bag(in_rosbag_file, 'r').read_messages():
+
+        # set seq for left camera_info message
+        if topic == "kitti_stereo/left/image_rect":
+          timestamp = msg.header.stamp.to_sec()
+          timestamps.append(timestamp)
+
       with open(in_kitti_gt_file) as kitti_gt_file:
-        with open(in_times_file) as times_file:
           with open(out_new_format, 'w') as outputFile:
             writer = csv.writer(outputFile, delimiter=' ')
             kitti_gt_reader = csv.reader(kitti_gt_file, delimiter=' ')
-            times_file_reader = csv.reader(times_file)
 
-            index_pose = 0
             # each line in the kitti is a pose
-            for kitti_gt_line, times_line in zip(kitti_gt_reader, times_file_reader):
-
-                # get timestamp
-#                timestamp = times_line
-#                index_pose += 1
-                timestamp = times_line
+            for kitti_gt_line, timestamp in zip(kitti_gt_reader, timestamps):
 
                 # get pose
                 kitti_pose = np.array(kitti_gt_line)
@@ -76,12 +78,14 @@ def main():
                 quaternion = np.array([quaternion[1], quaternion[2], quaternion[3], quaternion[0]])
 
                 # create TUM format
-                newRow = timestamp + list(traslation) + list(quaternion)
+                newRow = [timestamp] + list(traslation) + list(quaternion)
                 writer.writerow(newRow)
+
+      # check that ground-truth and rosbag timestamps have the same length
+      assert kitti_gt_reader.line_num == len(timestamps)
 
       # close files
       kitti_gt_file.close()
-      times_file.close()
       outputFile.close()
 
 if __name__=="__main__":
